0387a9428d35ab6c0d8da0dfb2a55ed599e64aaf
Leonardo C Villalobos <75503696+villaleo@users.noreply.github.com>
2021-01-31 15:10:41 -0800

    Add documentation to methods

--- chatbot/Chatbot_runner.java
+++ chatbot/Chatbot_runner.java
@@ -1,46 +1,63 @@
+// ***************************************************************
+// File: Chatbot_runner.java
+// Authors: Carlos Gonzales, Joseph Sloan, Leonardo Villalobos
+// Last Modified : 1 / 31 / 2021
+// Description : The class that contains the main method to execute
+// the chatbot.
+// ***************************************************************
 import java.util.Scanner;
 import java.util.*;
 
-/**
- * The runner class.
- * @vers : 4.0.2.18.20
- */
-public class Chatbot_runner {
-    
-    public static void main(String[] bot) {
-        Chatbot kahreéenngh = new Chatbot();
-        System.out.println (kahreéenngh.getGreeting());
+// Main class that contains the main() method to execute the program.
+public class Chatbot_runner
+{
+    // Main method that initializes the bot and contains various
+    // conditions to check for words found in the user's response.
+    public static void main(String[] bot)
+    {
+        Chatbot steve = new Chatbot();
+        System.out.println (steve.getGreeting());
         
         Scanner in = new Scanner (System.in);
         String statement = in.nextLine();
-        final String[] GOODBYES = {"See yah later.", "Bye now!", "byeee!", "Goodbye, friend."};
-        final String[] LEAVE_PROF = {"I don't have to listen to this.", "Enough!", "Now you've done it."};
+        final String[] GOODBYES = 
+        {
+            "See yah later.", "Bye now!", "byeee!", "Goodbye, friend."
+        };
+        final String[] LEAVE_PROF =
+        {
+            "I don't have to listen to this.", "Enough!", "Now you've done it."
+        };
         
-        while (!statement.toLowerCase().equals("bye")) {
-            System.out.println (kahreéenngh.getResponse(statement));
+        // Conditions to check when to terminate the program.
+        while (!statement.toLowerCase().equals("bye"))
+        {
+            System.out.println (steve.getResponse(statement));
             statement = in.nextLine();
             Chatbot var = new Chatbot();
-            if (var.unlucky_leave == 2) {
+            if (var.unlucky_leave == 2)
+            {
                 System.out.println("Yea that's nice.");
                 System.out.println("I'm leaving now, bye.");
-                System.out.println("Kahreéenngh has left the chat.");
+                System.out.println("Steve has left the chat.");
                 break;
             }
-            if (statement.toLowerCase().equals("fortnite")) {
-                System.out.println("Kahreéenngh has left the chat.");
+            if (statement.toLowerCase().equals("fortnite"))
+            {
+                System.out.println("Steve has left the chat.");
                 break;
             }
             Chatbot var2 = new Chatbot();
-            if (var2.prof_counter == 2) {
+            if (var2.prof_counter == 2)
+            {
                 System.out.println(LEAVE_PROF[(int)(Math.random() * 2)]);
-                System.out.println("Kahreéenngh has left the chat.");
+                System.out.println("Steve has left the chat.");
                 break;
             }
-            if (statement.toLowerCase().equals("bye")) {
+            if (statement.toLowerCase().equals("bye"))
+            {
                 System.out.println(GOODBYES[(int)(Math.random() * 3)]);
             }
-            
         }
     }
-
 }
